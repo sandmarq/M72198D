bb37347916f081aaf6fc80cc8d127fe1d6fa727c
Sandrine Marquis <sandrine.marquis30@gmail.com>
2017-01-31 20:00:11 -0500

    some addjustment for +futur func

--- examples/simple/simple.ino
+++ examples/simple/simple.ino
@@ -6,41 +6,20 @@
 #define NUMDISP 1
 
 Max7219 display1(MAX7219_DIN, MAX7219_CS, MAX7219_CLK, NUMDISP);
+String a="31012017";
 
 void setup() {
   Serial.begin(115200);
-  // put your setup code here, to run once:
-//  initialise();
-//  output(0x0f, 0x00); //display test register - test mode off
-  display1.init(); // initialize and display test register - test mode off
+  //  initialise();
+  display1.init();
   Serial.println("init");
   delay(250);
 }
 
 void loop() {
-//  output(0x0c, 0x01); //shutdown register - normal operation
-  display1.setaddress(MAX7212_REG_ADD_SHTD);
-  display1.setdata(MAX7219_SHTD_REG_FOR_NOR);
-  display1.output();
-  Serial.println("shutdown register - normal operation");
-
-  // output(0x0b, 0x07); //scan limit register - display digits 7 thru 0
-  display1.setaddress(MAX7212_REG_ADD_SCLT);
-  display1.setdata(MAX7219_NUM_DIGIT_8);
-  display1.output();
-  Serial.println("scan limit register - display digits 7 thru 0");
-
-//  output(0x0a, 0x0f); //intensity register - max brightness
-  display1.setaddress(MAX7212_REG_ADD_BRIG);
-  display1.setdata(MAX7219_INTE_MAX);
-  display1.output();
-  Serial.println("intensity register - max brightness");
-
-//  output(0x09, 0xff); //decode mode register - CodeB decode all digits
-  display1.setaddress(MAX7212_REG_ADD_DECM);
-  display1.setdata(MAX7219_DEC_MOD_REG_CB70);
-  display1.output();
-  Serial.println("decode mode register - CodeB decode all digits");
+  for(int x=0;x<a.length();x++){
+    Serial.println(a[x]);
+  }
 
 //  output(0x08, 0x0c); //digit 7 (leftmost digit) data
   display1.setaddress(MAX7212_REG_ADD_DIG7);
@@ -50,7 +29,7 @@ void loop() {
 
 //  output(0x07, 0x0b);
   display1.setaddress(MAX7212_REG_ADD_DIG6);
-  display1.setdata(MAX7219_CODEB_FONT_REG_0);
+  display1.setdata(MAX7219_CODEB_FONT_REG_1);
   display1.output();
 
 
--- m72198d.cpp
+++ m72198d.cpp
@@ -24,14 +24,40 @@ void Max7219::setdata(byte data)
   _data = data;
 }
 
+/*
+initialize the display to normal operation, with 8 digits, maximum brightness,
+with CodeB decode on all digits.
+*/
 void Max7219::init()
 {
   digitalWrite(_max7219cs, HIGH);
   pinMode(_max7219din, OUTPUT);
   pinMode(_max7219cs, OUTPUT);
   pinMode(_max7219clk, OUTPUT);
+
+  //  output(0x0f, 0x00); //display test register - test mode off
   setaddress(MAX7212_REG_ADD_DPTE);
   setdata(MAX7212_REG_ADD_NOOP);
+  output();
+
+  //  output(0x0c, 0x01); //shutdown register - normal operation
+  setaddress(MAX7212_REG_ADD_SHTD);
+  setdata(MAX7219_SHTD_REG_FOR_NOR);
+  output();
+
+  // output(0x0b, 0x07); //scan limit register - display digits 7 thru 0
+  setaddress(MAX7212_REG_ADD_SCLT);
+  setdata(MAX7219_NUM_DIGIT_8);
+  output();
+
+  //  output(0x0a, 0x0f); //intensity register - max brightness
+  setaddress(MAX7212_REG_ADD_BRIG);
+  setdata(MAX7219_INTE_MAX);
+  output();
+
+  //  output(0x09, 0xff); //decode mode register - CodeB decode all digits
+  setaddress(MAX7212_REG_ADD_DECM);
+  setdata(MAX7219_DEC_MOD_REG_CB70);
   output();
 }
 
